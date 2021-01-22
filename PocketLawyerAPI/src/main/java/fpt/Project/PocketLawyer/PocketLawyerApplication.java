3291c96f634d48b7ea3884eb136b89254e3302f0
cuong.tinvietsoft <cuong.tinvietsoft@gmail.com>
2021-01-22 18:52:52 +0700

    API

---
+++ PocketLawyerAPI/src/main/java/fpt/Project/PocketLawyer/PocketLawyerApplication.java
@@ -0,0 +1,13 @@
+package fpt.Project.PocketLawyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketLawyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketLawyerApplication.class, args);
+	}
+
+}
